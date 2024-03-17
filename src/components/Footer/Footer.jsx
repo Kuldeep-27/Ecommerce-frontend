d85fcf029a9317979e6968296f669729d1bb924b
Kuldeeep Khutela <khutelakuldeep52@gmail.com>
2024-03-17 20:50:57 +0530

    Final

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.scss"
 const Footer = () => {
     return (
         <div className="footer-container">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui nemo fuga velit consectetur similique ratione culpa autem quisquam? Porro, voluptatem consequatur repellendus quis eius veniam vitae deserunt corrupti, illo dolores ex optio, voluptatum exercitationem facere autem? Earum voluptates officiis vero exercitationem iste assumenda eum maiores rem aspernatur deserunt? Ea, provident!
+         <p style={{color:"white"}}>This is Footer</p>
         </div>
     )
 }
