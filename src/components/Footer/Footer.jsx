c3023bfae372d1cc2ea891558107c63bce553edd
Kuldeeep Khutela <khutelakuldeep52@gmail.com>
2024-03-17 20:44:39 +0530

    Finall

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.scss"
 const Footer = () => {
     return (
         <div className="footer-container">
-          Footer Section
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui nemo fuga velit consectetur similique ratione culpa autem quisquam? Porro, voluptatem consequatur repellendus quis eius veniam vitae deserunt corrupti, illo dolores ex optio, voluptatum exercitationem facere autem? Earum voluptates officiis vero exercitationem iste assumenda eum maiores rem aspernatur deserunt? Ea, provident!
         </div>
     )
 }
